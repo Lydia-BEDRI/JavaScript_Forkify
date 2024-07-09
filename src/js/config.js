0cbf1a453f0276b7f408b81f67d6345147077cc9
Lydia-BEDRI <bedrilydia118@gmail.com>
2024-07-09 12:09:29 +0200

    add API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '0abf116c-b901-4723-bf29-a8bccdf20c7d';
 export const MODAL_CLOSE_SEC = 2.5;
